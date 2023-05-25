3afbad51a6c01b0a66402bbc16afd05979368367
seuriseuljjeok <heeseul09@naver.com>
2023-05-25 16:31:47 +0900

    feat:mainpage 로그인&모달 UI 구현

--- MyIceCreamBox_Front/App.js
+++ MyIceCreamBox_Front/App.js
@@ -1,3 +1,12 @@
-import App from './src/components'
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigation from './src/stack';
+
+const App = () => {
+    return(
+        <NavigationContainer>
+                <StackNavigation/>
+        </NavigationContainer>
+    )
+}
 
 export default App;
