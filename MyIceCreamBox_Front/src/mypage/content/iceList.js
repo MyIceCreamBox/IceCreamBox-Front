0d467228b51676d7ba68926e5ee838c1919af61a
phonil <phonil@naver,com>
2023-06-03 16:29:54 +0900

    refactor: mypage 코드 리팩토링

--- MyIceCreamBox_Front/src/mypage/content/iceList.js
+++ MyIceCreamBox_Front/src/mypage/content/iceList.js
@@ -1,22 +1,76 @@
-import { Image } from 'react-native';
-// 굳이 딸기콘인 이유?
-import Strawberry from '../../../assets/imgs/strawberry.png';
+import { View, FlatList, Text } from 'react-native';
 import { StyleSheet } from 'react-native';
 import { width, height } from '../../global/dimension';
-import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-const Have = () => {
-  return <Image source={Strawberry} style={styles.con} />;
+import DBar from './dBar';
+import PBar from './pBar';
+import OBar from './oBar';
+
+const IceList = () => {
+  const data = [
+    { id: '1', name: 'Item 1' },
+    { id: '2', name: 'Item 2' },
+    { id: '3', name: 'Item 3' },
+    { id: '4', name: 'Item 4' },
+    { id: '5', name: 'Item 5' },
+    { id: '6', name: 'Item 6' },
+    { id: '7', name: 'Item 7' },
+    { id: '8', name: 'Item 8' },
+    { id: '9', name: 'Item 9' },
+  ];
+  console.log(data[0].id);
+
+  const renderItem = ({ item }) => (
+    <View style={styles.iceList}>
+      {item.id === '1' ? (
+        <OBar />
+      ) : item.id === '2' ? (
+        <DBar />
+      ) : item.id === '3' ? (
+        <PBar />
+      ) : item.id === '4' ? (
+        <DBar />
+      ) : item.id === '5' ? (
+        <PBar />
+      ) : item.id === '6' ? (
+        <OBar />
+      ) : item.id === '7' ? (
+        <OBar />
+      ) : item.id === '8' ? (
+        <PBar />
+      ) : item.id === '9' ? (
+        <DBar />
+      ) : (
+        <Text>없어요 없다구요</Text>
+      )}
+    </View>
+  );
+
+  return (
+    <View style={styles.back}>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-  con: {
-    width: width * 0.205,
-    height: height * 0.105,
-    // aspectRatio: 80 / 126,
-    resizeMode: 'contain',
-    top: hp('1.8%'),
+  back: {
+    width: width * 0.821,
+    height: height * 0.533,
+    borderRadius: 20,
+    backgroundColor: '#FFFFEE',
+    alignItems: 'center',
+  },
+  iceList: {
+    // margin: width * 0.077,
+    margin: width * 0.1,
+    justifyContent: 'space-evenly',
   },
 });
 
-export default Have;
+export default IceList;
